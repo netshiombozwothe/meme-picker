27ebe82c066d93b90c7766845b5f55146404fda4
netshiombozwothe <Zwothe118@gmail.com>
2025-07-28 22:21:51 +0200

    implement renderEmotionRadios

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
+import {catsData} from '/myData.js'
 
-import {catsData} from '/data.js'
+const emotionRadios  = document.getElementById('emotion-radios')
 
-const emotionRadios = document.getElementById('emotion-radios')
+renderEmotionRadios(catsData)
+emotionRadios.addEventListener('change' , highlightCheckedOption)
 
-renderEmotionRadios()
+function getEmotionTagsArray(){
+const emotionsArray = []
+
+for(let cats of catsData){
+    for(let emotions of cats.emotionTags){
+        if (!emotionsArray.includes(emotions)){
+        emotionsArray.push(emotions)
+        }
+    }
+}
+return emotionsArray
+}
 
 function renderEmotionRadios(){
+    let radioItems = ''
+    const emotions = getEmotionTagsArray()
 
-    for (let cats of catsData){
-        for (let emotions of cats.emotionTags){
-             emotionRadios.innerHTML +=
-                  `<div class="radio" id="radio">
-                  <label for="${emotions}">${emotions}</label>
-                  <input type="radio" class="change-color" id="${emotions}"></input>
-
-                 </div>`
-        }
-
+    for (let emotion of emotions){
+        radioItems += `
+        <div class="radio">
+        <label for="${emotion}">${emotion}</label>
+        <input type="radio"
+        id="${emotion}"
+        name="emotions"
+        value="${emotion}"
+        >
+        </div>
+        `
     }
+    emotionRadios.innerHTML = radioItems
+}
+function highlightCheckedOption(event){
+    const radios = document.getElementsByClassName('radio')
+     for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+        document.getElementById(event.target.id).parentElement.classList.add('highlight')
+
 }
